7747e139fdb7fe7748f6efc41f45862c3b062fd6
Socket <isra.mullen86@gmail.com>
2013-12-01 17:12:23 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>FFVIII Card Game</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css" media="screen" />
+        <script type="text/javascript" src=js/createjs-2013.09.25.min.js></script>
+    </head>
+    <body>
+        <canvas id="mainStage">
+        </canvas>
+    </body>
+</html>
